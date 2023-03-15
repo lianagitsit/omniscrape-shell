6df8b5a4ab1978248b9a0cb12a3c7b73c66bfb36
Liana Mancini <liana.mancini@gmail.com>
2023-03-15 08:49:27 -0400

    server setup

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HIYA!!
+          Very good, everything is up and running.
         </p>
         <a
           className="App-link"
